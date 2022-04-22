1c95ea60ce9c3376732616d14274838ca160eaf8
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-22 14:04:28 +0300

    task solving

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ function logs(x , a, b){
 }
 
 
+// Multiplication table for number Your goal is to return multiplication table for number that is always an integer from 1 to 10. You can use \n in string to jump to the next line. Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
 
 function multiTable(number) {
 	i = 1;
