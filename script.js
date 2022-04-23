740c0f4ceb8e5d550f2bef71ebdb5d99fc716dde
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-23 20:56:32 +0300

    task solving

--- script.js
+++ script.js
@@ -109,3 +109,11 @@ const areaOrPerimeter = function (l, w) {
 	}
 	return l * w;
 }
+
+
+// Remove String Spaces Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+	x = x.split(" ").join("");
+	return x;
+}
