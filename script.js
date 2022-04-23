bc352daf82363cda47ef44b346071ad12a1847c2
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-23 21:13:11 +0300

    task solving

--- script.js
+++ script.js
@@ -117,3 +117,14 @@ function noSpace(x) {
 	x = x.split(" ").join("");
 	return x;
 }
+
+
+// If you can't sleep, just count sheep!! Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num) {
+	let murmur = "";
+	for (var i = 0; i < num; i++) {
+		murmur += i + 1 + " sheep...";
+	}
+	return murmur;
+}
