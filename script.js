efef0ed90df82d23df1cfd5f351c78df73fd4499
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-23 20:46:23 +0300

    task solving

--- script.js
+++ script.js
@@ -98,3 +98,14 @@ function testEqual(val) {
 }
 
 console.log(testEqual(12));
+
+
+// Area or Perimeter You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function (l, w) {
+	if (l != w) {
+		return (l + w) * 2;
+	}
+	return l * w;
+}
