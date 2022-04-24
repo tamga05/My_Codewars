470743a0085a836e22c8b942fbfc429f1245fea9
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-24 14:13:02 +0300

    task solving

--- script.js
+++ script.js
@@ -176,6 +176,32 @@ function typeValidation(variable, type) {
 
 function binToDec(bin) {
 	let digit = parseInt(bin, 2);
-
 	return digit;
+}
+
+
+// Fundamentals: Return Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+function add(a, b){
+    return (a + b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function mod(a, b){
+    return a % b;
+}
+
+function exponent(a, b){
+    return a ** b;
+}
+
+function subt(a, b){
+    return (a - b);
 }
