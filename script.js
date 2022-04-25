4782e9547546cb0b90a14390521dfc47f867c2a6
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-04-25 15:31:28 +0300

    task solving

--- script.js
+++ script.js
@@ -242,3 +242,17 @@ function addBinary(a, b) {
 }
 
 console.log(addBinary(5, 3));
+
+
+// Функция возвращает количество Баллов студентов в зависимости от оценки на экзамене и количества выполненных проектов
+
+function finalGrade(exam, projects) {
+	if (exam > 90 || projects > 10) {
+		return 100;
+	} else if (exam > 75 && projects >= 5) {
+		return 90;
+	} else if (exam > 50 && projects >= 2) {
+		return 75;
+	}
+	return 0;
+}
