e5b0cd5fcd7a00293d40cafe9f3ee8d7e7b5f504
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-05 15:57:17 +0300

    task solving

--- script.js
+++ script.js
@@ -276,9 +276,45 @@ var humanYearsCatYearsDogYears = function (humanYears) {
 };
 
 
-// Third Angle of a Triangle You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
+// Third Angle of a Triangle. You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
 
 function otherAngle(a, b) {
 	return 180 - (a + b);
+}
+
+// Return the day. Complete the function which returns the weekday according to the input number:
+
+function whatday(num) {
+	if (num == 1) {
+		return "Sunday";
+	} else if (num == 2) {
+		return "Monday";
+	} else if (num == 3) {
+		return "Tuesday";
+	} else if (num == 4) {
+		return "Wednesday";
+	} else if (num == 5) {
+		return "Thursday";
+	} else if (num == 6) {
+		return "Friday";
+	} else if (num == 7) {
+		return "Saturday";
+	} else {
+		return "Wrong, please enter a number between 1 and 7";
+	}
+}
+
+
+// Opposites Attract. Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love. Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+	if (
+		(flower1 % 2 == 0 && flower2 % 2 != 0) ||
+		(flower1 % 2 != 0 && flower2 % 2 == 0)
+	) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
