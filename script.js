7340ed9dff499a6fddc365c9c296c1ad9801755d
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-07 00:22:01 +0300

    task solving

--- script.js
+++ script.js
@@ -333,3 +333,16 @@ function unusualFive() {
 	return five.length;
 }
 
+
+// Ensure question. Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+function ensureQuestion(s) {
+	if (s[s.length - 1] != "?") {
+		return `${s}?`;
+	} else {
+		return s;
+	}
+}
+
+console.log(ensureQuestion('Разве нет'));
+console.log(ensureQuestion('Почему?'));
