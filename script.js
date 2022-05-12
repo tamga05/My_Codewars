0c8ae1303878ae3c294bc24d339359737d1b4f8c
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-12 09:48:00 +0300

    task solving

--- script.js
+++ script.js
@@ -435,3 +435,16 @@ function index(array, n) {
 	return array[n] ** n || -1;
 }
 
+
+// Rock Paper Scissors!. Let's play! You have to return which player won! In case of a draw return Draw!.
+
+const rps = (p1, p2) => {
+    if (p1 == 'rock' && p2 == 'scissors' || p1 == 'paper' && p2 == 'rock' || p1 == 'scissors' && p2 == 'paper') {
+        return 'Player 1 won!';
+    } else if (p1 == 'scissors' && p2 == 'rock' || p1 == 'rock' && p2 == 'paper' || p1 == 'paper' && p2 == 'scissors') {
+        return 'Player 2 won!';
+    } else {
+        return 'Draw!';
+    }
+};
+
