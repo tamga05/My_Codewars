a26c2d74efebeb3dd6a8c98899b6624bebb512fd
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-13 09:48:52 +0300

    task solving

--- script.js
+++ script.js
@@ -466,3 +466,13 @@ function greet() {
     return "hello world!";
 }
 
+
+// Even or Odd. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
