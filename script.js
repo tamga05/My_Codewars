a3c5add2ac0e254cd4dfd9b52d60557d8bdfe89d
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-16 00:04:30 +0300

    task solving

--- script.js
+++ script.js
@@ -509,3 +509,18 @@ function remove(string) {
         return string;
     }
 }
+
+
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+function saleHotdogs(n) {
+    if (n < 5) {
+        return 100 * n;
+    } else if (n >= 5 && n < 10) {
+        return 95 * n;
+    } else {
+        return 90 * n;
+    }
+}
+
+
