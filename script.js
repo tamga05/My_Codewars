85a1e77a86f769d55fb299761dd32cfe49e52a07
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-15 07:19:29 +0300

    task solving

--- script.js
+++ script.js
@@ -494,4 +494,18 @@ const stringToNumber = function (str) {
 };
 
 console.log(stringToNumber('50'));
-console.log(typeof(stringToNumber('50')));
+console.log(typeof (stringToNumber('50')));
+
+
+// Exclamation marks series #1: Remove an exclamation mark from the end of string. Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+function remove(string) {
+
+    let lastSign = string[string.length - 1];
+
+    if (lastSign === '!') {
+        return string.slice(0, -1);
+    } else {
+        return string;
+    }
+}
