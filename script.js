0412840a16d4a6e71704acad84055f830c565a20
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-17 00:07:56 +0300

    task solving

--- script.js
+++ script.js
@@ -524,3 +524,40 @@ function saleHotdogs(n) {
 }
 
 
+// Training JS #2: Basic data types--Number. Making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+
+const v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250;
+
+function equal1() {
+    const a = v1;
+    const b = v1;
+    return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2() {
+    const a = v3;   //set number value to a
+    const b = v1;   //set number value to b
+    return a - b;
+}
+
+function equal3() {
+    const a = v1;   //set number value to a
+    const b = v5;   //set number value to b
+    return a * b;
+}
+
+function equal4() {
+    const a = v4;   //set number value to a
+    const b = v5;   //set number value to b
+    return a / b;
+}
+
+function equal5() {
+    const a = v6;   //set number value to a
+    const b = v3;   //set number value to b
+    return a % b;
+}
+
+
+
