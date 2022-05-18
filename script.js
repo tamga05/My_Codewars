0a80e03aee31a25c824ccebdda360422af0ad2a5
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-18 10:49:40 +0300

    task solving

--- script.js
+++ script.js
@@ -560,4 +560,23 @@ function equal5() {
 }
 
 
+// Training JS #1: create your first JS function and print "Hello World!". mission 1:
+//
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+//
+// mission 2:
+//
+// use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+//
+// mission 3:
+//
+// type the console.log() in the next line(don't forget to put the str in the brackets).
+
+function helloWorld() {
+    const str = "Hello World!";
+    console.log(str);
+}
+
+
+
 
