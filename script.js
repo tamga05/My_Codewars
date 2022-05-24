6aac7c2071d4fc528e854aa1861c4af99ceb7d4b
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-24 22:35:12 +0300

    task solving

--- script.js
+++ script.js
@@ -640,6 +640,11 @@ function makeUpperCase(str) {
 }
 
 
+// Returning Strings. Make a function that will return a greeting statement that uses an input; your program should return, Hello, <name> how are you doing today?.
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
 
 
 
