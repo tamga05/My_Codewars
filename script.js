977a63f4826117ab67479430af2c8ed639ec6ee6
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-07 21:42:07 +0300

    task solving

--- script.js
+++ script.js
@@ -688,3 +688,14 @@ function enough(cap, on, wait) {
     return Math.max(wait + on - cap, 0);
 }
 
+
+//101 Dalmatians - squash the bugs, not the dogs!
+
+function howManyDalmatians(number) {
+
+    const dogs = ['Hardly any', 'More than a handful!', 'Woah that\'s a lot of dogs!', '101 DALMATIANS!!!'];
+
+    return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2];
+}
+
+console.log(howManyDalmatians(101));
