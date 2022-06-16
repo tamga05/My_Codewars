ac61a335f5afba9365bd27bb21f8a9a8891f2022
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-16 14:51:47 +0300

    task solving

--- script.js
+++ script.js
@@ -743,3 +743,16 @@ function boolToWord2(bool) {
 //     return "No";
 //   }
 // }
+
+
+// Are You Playing Banjo?
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0) === 'R' || name.charAt(0) === 'r') {
+        return `${name} plays banjo`;
+    } else {
+        return `${name} does not play banjo`;
+    }
+}
