5c73654e8f0f1f56021f70d1f506ffde9d96efe5
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-17 22:48:37 +0300

    task solving

--- script.js
+++ script.js
@@ -756,3 +756,10 @@ function areYouPlayingBanjo(name) {
         return `${name} does not play banjo`;
     }
 }
+
+
+// String repeat. Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr(n, s) {
+    return s.repeat(n);
+}
