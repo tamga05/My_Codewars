bd387af6b9a03373cb001067c79bbf3a6442212f
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-20 10:46:44 +0300

    task solving

--- script.js
+++ script.js
@@ -770,3 +770,11 @@ function repeatStr(n, s) {
 function square(x) {
     return x * x;
 }
+
+
+// You Can't Code Under Pressure #1. Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    i = i * 2;
+    return i;
+}
