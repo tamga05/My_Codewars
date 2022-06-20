3c8f86681658e9ccae392a90aab20c63d2d8e3de
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-21 01:25:40 +0300

    task solving

--- script.js
+++ script.js
@@ -778,3 +778,10 @@ function doubleInteger(i) {
     i = i * 2;
     return i;
 }
+
+
+// Will you make it?
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump === mpg * fuelLeft ? true : false;
+};
