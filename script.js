2ffaaeba1ed2fa7e89d340e8ec71e3807c54e17f
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-06 00:42:58 +0300

    task solving

--- script.js
+++ script.js
@@ -862,3 +862,24 @@ function rainAmount(mm) {
         return 'Your plant has had more than enough water for today!';
     }
 }
+
+console.log(typeof NaN);
+
+console.log(typeof null);
+
+
+
+//Unfinished Loop - Bug Fixing #1. Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number) {
+    let newArray = [];
+
+    for ( let i = 1; i <= number; i++) {
+        newArray.push(i);
+    }
+
+    return newArray;
+}
+
+console.log(createArray(15));
+
