ba134ff31f517626add46ac20eb8f4ca7e6554d3
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-09-30 10:22:54 +0300

    task solving

--- script.js
+++ script.js
@@ -1048,6 +1048,14 @@ function updateLight(current) {
 
 // Hex to Decimal. Complete the function which converts hex number (given as a string) to a decimal number.
 
-function hexToDec(hexString){
+function hexToDec(hexString) {
     return parseInt(hexString, 16);
+}
+
+
+// Take the Derivative. This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+function derive(coefficient, exponent) {
+    return `${coefficient * exponent}x^${exponent - 1}`;
 }
