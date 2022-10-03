ffbbd86779e4dfb71e8472571db5d49eef17ce53
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-10-03 09:38:11 +0300

    task solving

--- script.js
+++ script.js
@@ -1085,3 +1085,14 @@ function magNumber(info) {
 const cubeChecker = function (volume, side) {
     return volume / side ** 2 === side && side > 0;
 };
+
+
+// Pillars. There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+function pillars(numPill, dist, width) {
+    return numPill === 1 ? 0 : ((numPill - 2) * width) + ((100 * dist) * (numPill - 1));
+}
