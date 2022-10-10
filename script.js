3b27ee3ff12e336bae09f6b650650ea87633d419
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-10-10 15:12:15 +0300

    task solving

--- script.js
+++ script.js
@@ -1110,3 +1110,10 @@ function invert(array) {
         return each * -1;
     });
 }
+
+
+// Duck Duck Goose. Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen
+// Player(name is a property of Player objects, e.g Player.name).
+
+const duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name;
+
