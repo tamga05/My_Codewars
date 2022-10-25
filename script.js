15102cd3936bb1881ed1cb8f6cb7c9ef93a477ba
tamga05 <tamga05@gmail.com>
2022-10-25 13:59:13 +0300

    task solving

--- script.js
+++ script.js
@@ -1133,3 +1133,20 @@ function reverseWords(str) {
 function any(arr, fun) {
     return arr.some(fun);
 }
+
+
+// DNA to RNA Conversion. Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+// Create a function which translates a given DNA string into RNA.
+
+function DNAtoRNA(dna) {
+    let str = '';
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === 'T') {
+            str += 'U';
+        } else {
+            str += dna[i];
+        }
+    }
+    return str;
+}
