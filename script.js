9946e49d27c9687836a8426766aecfc1d6b04edb
tamga05 <tamga05@gmail.com>
2022-10-30 08:13:44 +0300

    task solving

--- script.js
+++ script.js
@@ -1197,7 +1197,17 @@ function integrate(coefficient, exponent) {
 // There will never be a case when both are empty as nothing is going on!!
 // Note: The character is a string of length 1 or an empty string.
 
-function contamination(text, char){
-    return char.repeat(text.length)
+function contamination(text, char) {
+    return char.repeat(text.length);
+}
+
+
+// Geometry Basics: Distance between points in 2D. This series of katas will introduce you to basics of doing geometry with computers.
+// Point objects have x and y attributes (X and Y in C#) attributes.
+// Write a function calculating distance between Point a and Point b.
+// Tests round answers to 6 decimal places.
+
+function distanceBetweenPoints(a, b) {
+    return Math.hypot(a.x - b.x, a.y - b.y);
 }
 
