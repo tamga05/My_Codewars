b1d063ac7516149286b219b3cc9577378ab0ba2f
tamga05 <tamga05@gmail.com>
2022-11-17 15:46:58 +0300

    task solving

--- script.js
+++ script.js
@@ -1331,3 +1331,16 @@ Dog.prototype.bark = function () {
 };
 
 const scoobydoo = new Dog('Great Dane');
+
+
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence. Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+//
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s) {
+    return s.replace(/[aeoiu]/ig, '!');
+}
