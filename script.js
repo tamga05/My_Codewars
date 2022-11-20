dbdd1e61aefe8eabf4613ae5699f51a4f5b8ecd0
tamga05 <tamga05@gmail.com>
2022-11-20 09:49:52 +0300

    task solving

--- script.js
+++ script.js
@@ -1368,7 +1368,7 @@ function sixToast(num) {
 }
 
 
-// Holiday VIII - Duty Free. The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+// Holiday VIII - Duty Free. The purpose of this kata is to work out just how many bottles of duty-free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
 //
 // You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
 //
@@ -1378,4 +1378,15 @@ function sixToast(num) {
 
 function dutyFree(normPrice, discount, hol) {
     return (Math.floor(hol / normPrice / discount * 100));
+}
+
+
+// Beginner - Lost Without a Map. Given an array of integers, return a new array with each value doubled.
+//
+// For example:
+//
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+    return x.map(n => n * 2);
 }
