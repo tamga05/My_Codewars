9c0199cde05fc883657e31446adda6d7ea36c87c
tamga05 <tamga05@gmail.com>
2022-11-22 12:23:12 +0300

    task solving

--- script.js
+++ script.js
@@ -1407,3 +1407,24 @@ function maps(x) {
 function strCount(str, letter) {
     return str.split(letter).length - 1;
 }
+
+
+// Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right. Remove n exclamation marks in the sentence from left to right. n is positive integer.
+//
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+function remove(s, n) {
+    for (let i = 0; i < n; i++) s = s.replace('!', '');
+    return s;
+}
