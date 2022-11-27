8eed15ce2bded3ce2290a069c3e236321bdbfc2c
tamga05 <tamga05@gmail.com>
2022-11-27 12:21:18 +0300

    task solving

--- script.js
+++ script.js
@@ -1461,3 +1461,16 @@ function firstNonConsecutive(arr) {
 function check(a, x) {
     return a.includes(x);
 }
+
+
+// Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+//
+// Examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
+
+const remove = s => `${s.replace(/!+/g, '')}!`;
