93877f91ee1fbb95d9fa02d998527c78398e66c7
tamga05 <tamga05@gmail.com>
2022-12-02 12:10:52 +0300

    task solving

--- script.js
+++ script.js
@@ -1522,3 +1522,14 @@ function distinct(a) {
     return [...new Set(a)];
 }
 
+
+// Convert a string to an array. Write a function to split a string and convert it into an array of words.
+//
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+//
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+    return string.split(' ');
+}
