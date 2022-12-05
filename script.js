b106bab471e6dceed5bd273a5d3ad3576b655558
tamga05 <tamga05@gmail.com>
2022-12-05 09:58:11 +0300

    task solving

--- script.js
+++ script.js
@@ -1553,3 +1553,13 @@ function gooseFilter(birds) {
     const geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'];
     return birds.filter(b => !geese.includes(b));
 }
+
+
+// You only need one - Beginner.
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//
+// Array can contain numbers or strings. X can be either.
+//
+// Return true if the array contains the value, false if not.
+
+const check = (a, x) => a.includes(x);
