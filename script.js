497652fc514fdb8e7ebf166a62de234b548080f4
tamga05 <tamga05@gmail.com>
2022-12-16 09:23:18 +0300

    task solving

--- script.js
+++ script.js
@@ -1782,3 +1782,21 @@ const isValid = formula => {
 function toBinary(n) {
     return +n.toString();
 }
+
+
+// Is there a vowel in there?
+
+// DESCRIPTION:
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+//
+// If they are, change the array value to a string of that vowel.
+//
+// Return the resulting array.
+
+function isVow(a) {
+    return a.map(
+        charCode =>
+            /[aeiou]/.test(String.fromCharCode(charCode))
+                ? String.fromCharCode(charCode)
+                : charCode);
+}
