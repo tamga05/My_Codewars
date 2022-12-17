b014180e45b499e2efd5f799b7a349fb94f95447
tamga05 <tamga05@gmail.com>
2022-12-17 08:10:11 +0300

    task solving

--- script.js
+++ script.js
@@ -1800,3 +1800,24 @@ function isVow(a) {
                 ? String.fromCharCode(charCode)
                 : charCode);
 }
+
+
+// UEFA EURO 2016.
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+//
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+function uefaEuro2016(teams, scores) {
+    const teamA = teams[0];
+    const teamB = teams[1];
+
+    if (scores[0] > scores[1]) {
+        return 'At match ' + teamA + ' - ' + teamB + ', ' + teamA + ' won!';
+    } else if (scores[0] < scores[1]) {
+        return 'At match ' + teamA + ' - ' + teamB + ', ' + teamB + ' won!';
+    } else if (scores[0] === scores[1]) {
+        return 'At match ' + teamA + ' - ' + teamB + ', teams played draw.';
+    }
+}
