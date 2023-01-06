3166250467c932bcfa5ab064d7f70ce951ec78f4
tamga05 <tamga05@gmail.com>
2023-01-07 00:04:00 +0300

    task solving

--- script.js
+++ script.js
@@ -2108,3 +2108,21 @@ const yearDays = year =>
     `${year} has ${
         (!(year % 100) && year % 400) || year % 4 ? '365' : '366'
     } days`;
+
+
+// Pick a set of first elements.
+
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+//
+// If n == 0 return an empty sequence []
+//
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+function first(arr, n = 1) {
+    return arr.slice(0, n);
+}
