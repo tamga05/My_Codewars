2fa0fa74c324d1f96fce14dcf4458433149b5d1f
tamga05 <tamga05@gmail.com>
2023-03-09 22:22:41 +0300

    task solving

--- script.js
+++ script.js
@@ -2127,6 +2127,7 @@ function first(arr, n = 1) {
     return arr.slice(0, n);
 }
 
+
 // Counting sheep...
 
 //Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
@@ -2183,3 +2184,29 @@ const checkExam = (answers, responses) => {
 
     return score > 0 ? score : 0;
 };
+
+
+// Closest elevator
+
+// DESCRIPTION:
+// Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+//
+// left - The current floor of the left elevator
+// right - The current floor of the right elevator
+// call - The floor that called an elevator
+// It should return the name of the elevator closest to the called floor ("left"/"right").
+//
+// In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+//
+// You can assume that the inputs will always be valid integers between 0-2.
+//
+// Examples:
+//
+// elevator(0, 1, 0); // => "left"
+// elevator(0, 1, 1); // => "right"
+// elevator(0, 1, 2); // => "right"
+// elevator(0, 0, 0); // => "right"
+// elevator(0, 2, 1); // => "right"
+
+const elevator = (left, right, call) => Math.abs(call - left) < Math.abs(call - right) ? 'left' : 'right';
+
