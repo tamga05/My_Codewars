d8eb63b3aac22bc3e679e69ab7a21170a9c11cad
tamga05 <tamga05@gmail.com>
2023-03-14 23:13:01 +0300

    task solving

--- script.js
+++ script.js
@@ -2258,3 +2258,17 @@ function seatsInTheater(nCols, nRows, col, row) {
 function litres(time) {
     return Math.floor(time / 2);
 }
+
+
+// Sort and Star
+
+/*
+Description:
+You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+
+function twoSort(s) {
+    return s.sort()[0].split('').join('***');
+}
