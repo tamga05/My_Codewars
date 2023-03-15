4d471148575d767becceb21e9972346340c057f3
tamga05 <tamga05@gmail.com>
2023-03-15 14:19:28 +0300

    task solving

--- script.js
+++ script.js
@@ -2272,3 +2272,19 @@ You should not remove or add elements from/to the array.
 function twoSort(s) {
     return s.sort()[0].split('').join('***');
 }
+
+
+// String cleaning
+
+/*
+Description:
+Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text. For example:
+stringClean('! !') == '! !'
+stringClean('123456789') == ''
+stringClean('This looks5 grea8t!') == 'This looks great!'
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+*/
+
+function stringClean(s) {
+    return s.replace(/\d/g, '');
+}
