7c3fde5df30222fda1cddbb1396e75d66b5ccb04
tamga05 <tamga05@gmail.com>
2023-03-31 06:01:26 +0300

    task solving

--- script.js
+++ script.js
@@ -2533,3 +2533,15 @@ function validateHello(greetings) {
     let res = /(hello|ciao|salut|hallo|hola|ahoj|czesc)/i.test(greetings);
     return res;
 }
+
+
+//  Crash Override.
+
+const firstLetter = str => str[0].toUpperCase();
+
+const isValidName = name => /[a-z]/gi.test(name);
+
+const aliasGen = (fName, sName) =>
+    isValidName(fName[0]) && isValidName(sName[0])
+        ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(sName)]}`
+        : 'Your name must start with a letter from A - Z.';
