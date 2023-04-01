de4ce6b395ff64b5d505de1c1766db5ed0c632ec
tamga05 <tamga05@gmail.com>
2023-04-01 06:05:25 +0300

    task solving

--- script.js
+++ script.js
@@ -2537,6 +2537,21 @@ function validateHello(greetings) {
 
 //  Crash Override.
 
+/*
+Description:
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+I have created two objects that return a one word name in response to the first letter of your first name and one for the first letter of your surname.
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+Happy hacking!
+*/
+
+
 const firstLetter = str => str[0].toUpperCase();
 
 const isValidName = name => /[a-z]/gi.test(name);
@@ -2545,3 +2560,22 @@ const aliasGen = (fName, sName) =>
     isValidName(fName[0]) && isValidName(sName[0])
         ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(sName)]}`
         : 'Your name must start with a letter from A - Z.';
+
+
+// Thinkful - Number Drills: Blue and red marbles.
+
+/*
+Description:
+You and a friend have decided to play a game to drill your statistical intuitions. The game works like this:
+You have a bunch of red and blue marbles. To start the game you grab a handful of marbles of each color and put them into the bag, keeping track of how many of each color go in. You take turns reaching into the bag, guessing a color, and then pulling one marble out. You get a point if you guessed correctly. The trick is you only have three seconds to make your guess, so you have to think quickly.
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+the number of blue marbles you put in the bag to start
+the number of red marbles you put in the bag to start
+the number of blue marbles pulled out so far, and
+the number of red marbles pulled out so far.
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+*/
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    return (blueStart - bluePulled) / ((redStart - redPulled) + (blueStart - bluePulled));
+}
