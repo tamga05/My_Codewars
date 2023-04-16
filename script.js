75b724d02297c0364e3be54e56b72546a734cade
tamga05 <tamga05@gmail.com>
2023-04-16 08:58:23 +0300

    task solving

--- script.js
+++ script.js
@@ -2845,3 +2845,30 @@ function addFive(num) {
     const total = num + 5;
     return total;
 }
+
+
+// Messi's Goal Total.
+
+/*
+Description:
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 games
+Information
+Messi goal scoring statistics:
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Define
+Create three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals (javascript, C#) or total_goals (python, ruby). totalGoals/total_goals should store the sum of all of Messi's goals for this year.
+*/
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = 58;
