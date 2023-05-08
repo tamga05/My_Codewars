01ededb3a6f83c2df0318d4e5ea6cdad7922ea59
tamga05 <tamga05@gmail.com>
2023-05-08 15:01:12 +0300

    task solving

--- script.js
+++ script.js
@@ -3021,3 +3021,18 @@ function playerRankUp(points) {
     if (points >= 100) return 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.';
     return false;
 }
+
+
+// Keep up the hoop.
+
+/*
+Description:
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+-If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+-If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+*/
+
+function hoopCount(n) {
+    return n >= 10 ? 'Great, now move on to tricks' : 'Keep at it until you get it';
+}
