c4c8765a4f921f8f3719905517e0070120da61cf
tamga05 <tamga05@gmail.com>
2023-06-27 07:48:59 +0300

    task solving

--- script.js
+++ script.js
@@ -3967,3 +3967,15 @@ function pickIt(arr) {
     }
     return [odd, even];
 }
+
+
+// Remove exclamation marks.
+
+/*
+Description:
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '');
+}
