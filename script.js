06a6ef391fef94044714e96e03926e66db75fe10
tamga05 <tamga05@gmail.com>
2023-07-11 00:08:03 +0300

    task solving

--- script.js
+++ script.js
@@ -4353,3 +4353,18 @@ function match({minSalary}, {maxSalary}) {
     if (!minSalary || !maxSalary) throw new Error('');
     return minSalary * 0.9 <= maxSalary;
 }
+
+
+// Name Shuffler.
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+*/
+
+function nameShuffler(str) {
+    return str.split(' ').reverse().join(' ');
+}
+
+
