e52c75d0bb44bcabd4d10c12f9d3e57f82613eab
tamga05 <tamga05@gmail.com>
2023-07-13 00:04:04 +0300

    task solving

--- script.js
+++ script.js
@@ -4381,3 +4381,22 @@ function all(arr, fun) {
     return arr.every(fun);
 }
 
+
+// Find numbers which are divisible by given number.
+
+/*
+Description:
+Write a function which takes two arguments and returns all numbers which are divisible by given divisor. First argument is array of numbers and the second is divisor.
+
+Example
+divisibleBy([1,2,3,4,5,6], 2) == [2,4,6]
+divisibleBy([1,2,3,4,5,6], 2) == [2,4,6]
+*/
+
+function divisibleBy(numbers, divisor) {
+    return [...numbers].filter(num => num % divisor === 0);
+}
+
+
+
+
