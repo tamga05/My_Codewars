3fc782cd79ed7c73679c6965953e783de52c698b
tamga05 <tamga05@gmail.com>
2023-07-12 07:22:58 +0300

    task solving

--- script.js
+++ script.js
@@ -4368,3 +4368,16 @@ function nameShuffler(str) {
 }
 
 
+// Enumerable Magic #1 - True for All?
+
+/*
+Description:
+Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
+
+Here's a (Ruby) resource if you get stuck: http://www.rubycuts.com/enum-all
+*/
+
+function all(arr, fun) {
+    return arr.every(fun);
+}
+
