93dad031abad240988b8cd3b67c1a5552e7fe9f8
tamga05 <tamga05@gmail.com>
2023-07-14 00:44:15 +0300

    task solving

--- script.js
+++ script.js
@@ -4398,5 +4398,19 @@ function divisibleBy(numbers, divisor) {
 }
 
 
+// Wilson primes.
+
+/*
+Description:
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+const amIWilson = p => [5, 13, 563].indexOf(p) > -1;
+
+
 
 
