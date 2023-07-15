b4ae2bc580bba444563d6221464ffcd8ab816b83
tamga05 <tamga05@gmail.com>
2023-07-15 07:38:18 +0300

    task solving

--- script.js
+++ script.js
@@ -4412,5 +4412,20 @@ Your task is to create a function that returns true if the given number is a Wil
 const amIWilson = p => [5, 13, 563].indexOf(p) > -1;
 
 
+// Are arrow functions odd?
+
+/*
+Description:
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+odds([1,2,3,4,5]) #=> [1,3,5]
+*/
+
+function odds(values) {
+    // arrow it
+    return values.filter(x => x % 2 != 0);
+}
+
+
 
 
