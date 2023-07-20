372e00b0691315c60e5ee16f3f631ad191e67206
tamga05 <tamga05@gmail.com>
2023-07-20 12:54:14 +0300

    task solving

--- script.js
+++ script.js
@@ -4527,5 +4527,26 @@ function SafeInteger(n) {
 }
 
 
+// Template Strings.
+
+/*
+Description:
+Template Strings
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word
+' are '
+Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+*/
+
+const TempleStrings = function (obj, feature) {
+    return `${obj} are ${feature}`;
+};
+
+
 
 
