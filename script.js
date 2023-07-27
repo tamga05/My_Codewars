903ae3767018162f3d15712e5fa5a51110225585
tamga05 <tamga05@gmail.com>
2023-07-27 16:34:05 +0300

    task solving

--- script.js
+++ script.js
@@ -4800,3 +4800,25 @@ function barTriang([p1, p2], [p3, p4], [p5, p6]) {
     return [+((p1 + p5 + p3) / 3).toFixed(4), +((p2 + p4 + p6) / 3).toFixed(4)];
 
 }
+
+
+// Take an Arrow to the knee, Functionally.
+
+/*
+Description:
+Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+*/
+
+const ArrowFunc = function(arr) {
+    return arr.map( a =>String.fromCharCode(a) ).join(''); //Complete this function
+}
