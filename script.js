15737005f486b38f0302c0c8bdc786427bc8b2aa
tamga05 <tamga05@gmail.com>
2023-07-29 08:19:02 +0300

    task solving

--- script.js
+++ script.js
@@ -4849,3 +4849,25 @@ const isSquareRootInteger = num => Number.isInteger(Math.sqrt(num));
 
 const squareOrSquareRoot = array =>
     array.map(num => (isSquareRootInteger(num) ? Math.sqrt(num) : num ** 2));
+
+
+// Lexical this.
+
+/*
+Description:
+Lexical this
+Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+Output
+Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+*/
+
+const Person = function(){
+    const person = {
+        _name: "Leroy",
+        _friends: [],
+        fillFriends(f) {
+            this._friends.push(...f)}
+    }
+    return person;
+}
