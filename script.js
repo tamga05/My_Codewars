63fdd56ca038e784220b87dce06bd7e28adc5731
tamga05 <tamga05@gmail.com>
2023-08-10 09:22:45 +0300

    task solving

--- script.js
+++ script.js
@@ -5080,6 +5080,6 @@ If you need help, here's a reference:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 */
 
-// function take(arr, n) {
-//     return arr.slice(0, n);
-// }
+function take(arr, n) {
+    return arr.slice(0, n);
+}
