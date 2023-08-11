aaeddfcbf5964e6ef21806ce426a28296380b07e
tamga05 <tamga05@gmail.com>
2023-08-11 14:02:41 +0300

    task solving

--- script.js
+++ script.js
@@ -5083,3 +5083,38 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 function take(arr, n) {
     return arr.slice(0, n);
 }
+
+
+// Hello Happy Codevarrior!
+
+/*
+Description:
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+*/
+
+class Warrior {
+    constructor(newName) {
+        this.wName = newName;
+    }
+
+    name(newName) {
+        if (newName) {
+            this.wName = newName;
+        }
+        return this.wName;
+    }
+}
+
+Warrior.prototype.toString = function () {
+    return `Hi! my name's ${this.wName}`;
+};
+
