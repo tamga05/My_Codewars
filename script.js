6ecfe0621e78186fbe0d58dc6c2b3cd670908ded
tamga05 <tamga05@gmail.com>
2023-08-29 06:56:11 +0300

    task solving

--- script.js
+++ script.js
@@ -5837,9 +5837,9 @@ reverseList([1,2,3,4]) == [4,3,2,1]
 reverseList([3,1,5,4]) == [4,5,1,3]
 */
 
-// function reverseList(list) {
-//     return list.reverse();
-// }
+function reverseList(list) {
+    return list.reverse();
+}
 
 
 // What's up next?
