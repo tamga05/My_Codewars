89dd109b2b2411e95ee7038cf4c7b98f04c46203
tamga05 <tamga05@gmail.com>
2023-09-14 10:00:45 +0300

    task solving

--- script.js
+++ script.js
@@ -6157,6 +6157,30 @@ function eachCons(array, n) {
 // Push a hash/an object into array.
 
 // DESCRIPTION:
-//     You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
 
 items = [{a: 'b', c: 'd'}];
+
+
+// "this" is a problem.
+
+// We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
+//
+// We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it? A test fixture is also available
+//
+// function NameMe(first, last) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     return {name: this.firstName + ' ' + this.lastName};
+// }
+//
+// var n = new NameMe('John', 'Doe');
+// n.firstName //Expected: John
+// n.lastName //Expected: Doe
+// n.name //Expected: John Doe
+
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = first + ' ' + last;
+}
