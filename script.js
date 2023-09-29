a6c259ea066c5b39ee740b9b5e39898f82725faf
tamga05 <tamga05@gmail.com>
2023-09-29 09:44:39 +0300

    task solving

--- script.js
+++ script.js
@@ -6569,3 +6569,22 @@ function append(a, b) {
     a.next = append(a.next, b);
     return a;
 }
+
+
+// Build a square.
+
+// DESCRIPTION:
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+//
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+//
+// +++
+// +++
+// +++
+
+function generateShape(int) {
+    return Array(int).fill('+'.repeat(int)).join('\n');
+}
+
+
