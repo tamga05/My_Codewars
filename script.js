ebe859d9050cb4a4494fed31f1993a2e127bafa0
tamga05 <tamga05@gmail.com>
2023-09-30 07:32:46 +0300

    task solving

--- script.js
+++ script.js
@@ -6588,3 +6588,13 @@ function generateShape(int) {
 }
 
 
+// Average Scores.
+
+// DESCRIPTION:
+// Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+//
+// The array will never be empty.
+
+function average(scores) {
+    return Math.round(scores.reduce((first, next) => (first + next), 0) / scores.length);
+}
