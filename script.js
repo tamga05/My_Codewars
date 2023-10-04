9b109e7807bf51b7bb5d02d8eeaf921a8fb37a1b
tamga05 <tamga05@gmail.com>
2023-10-04 05:17:08 +0300

    task solving

--- script.js
+++ script.js
@@ -6677,3 +6677,30 @@ fix to 3 decimal places.
 function height(n) {
     return (2000000 * (1 - Math.pow(0.4, n + 1)) / (1 - 0.4)).toFixed(3);
 }
+
+
+// Chords.
+
+/*
+Description:
+In Western music, a major chord (major third) starts at a root note and adds the note 4 semitones and 7 semitones above it.
+
+A minor chord (minor third) starts at a root note and adds the note 3 semitones and 7 semitones above it.
+
+Given a root note root, please return an array of the major chord and the minor chord for that root.
+
+The notes are C, C#, D, D#, E, F, F#, G, G#, A, A#, B –– you are given this as a constant
+
+For a music theory kata from a different point of view (figuring out whether a given set of notes is a major chord or minor chord or neither), try this kata from aniametz.
+*/
+
+const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+function chords(root) {
+    const note = notes.indexOf(root);
+    const first = notes[note];
+    const second = notes[note + 4];
+    const third = notes[note + 7];
+    const fourth = notes[note + 3];
+    return [[first, second, third], [first, fourth, third]];
+}
