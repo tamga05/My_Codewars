d6b6c29248a6bef0d49b966c24bd2f454d195d85
tamga05 <tamga05@gmail.com>
2023-10-19 07:07:50 +0300

    task solving

--- script.js
+++ script.js
@@ -7119,3 +7119,17 @@ class Block {
         return 2 * (l * w + l * h + w * h);
     }
 }
+
+
+// Spacify.
+
+/*
+Description:
+Modify the spacify function so that it returns the given string with spaces between each letter. In this exercise, you can assume that only strings will be passed to your function.
+
+spacify('hello world') // h e l l o  w o r l d
+*/
+
+function spacify(str) {
+    return str.split('').join(' ');
+}
