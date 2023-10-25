c337f28f13e6eedb59c6c36fccc25646c8bf9968
tamga05 <tamga05@gmail.com>
2023-10-25 05:35:07 +0300

    task solving

--- script.js
+++ script.js
@@ -7295,11 +7295,11 @@ The solution to the example above should therefore appear like '1, 2: Bob made "
 
 */
 
-// function solve(a, b) {
-//     const [x, y] = a.reduce((p, a, i) => a == b[i] ? p : (++p[a > b[i] ? 0 : 1], p), [0, 0]);
-//     const m = (x == y) ? 'that looks like a "draw"! Rock on!' : (x > y) ? 'Alice made "Kurt" proud!' : 'Bob made "Jeff" proud!';
-//     return `${x}, ${y}: ${m}`;
-// }
+function solve(a, b) {
+    const [x, y] = a.reduce((p, a, i) => a == b[i] ? p : (++p[a > b[i] ? 0 : 1], p), [0, 0]);
+    const m = (x == y) ? 'that looks like a "draw"! Rock on!' : (x > y) ? 'Alice made "Kurt" proud!' : 'Bob made "Jeff" proud!';
+    return `${x}, ${y}: ${m}`;
+}
 
 
 // Ski Jump.
