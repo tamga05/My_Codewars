b7d1fe1d2e079e2d4ca997ca9ee80a39383802c5
tamga05 <tamga05@gmail.com>
2023-10-31 05:30:21 +0300

    task solving

--- script.js
+++ script.js
@@ -7453,11 +7453,11 @@ function isPowerOfTwo(n) {
 // largest_power(4) == 1
 // You may assume that the input to your function is always a positive integer.
 
-// function largestPower(n) {
-//     let res = 0;
-//     while (3 ** res < n) res++;
-//     return res - 1;
-// }
+function largestPower(n) {
+    let res = 0;
+    while (3 ** res < n) res++;
+    return res - 1;
+}
 
 
 // Powers of i.
