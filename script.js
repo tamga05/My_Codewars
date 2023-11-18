7e105408e93d55c7a1bff49d285163c4f53762b6
tamga05 <tamga05@gmail.com>
2023-11-18 04:10:29 +0300

    task solving

--- script.js
+++ script.js
@@ -7945,14 +7945,14 @@ For n = 25, the output should be halvingSum(n) = 47.
 The sum to be calculated is 25 + 12 + 6 + 3 + 1, which is 47.
 */
 
-// function halvingSum(n) {
-//     let score = 0;
-//     while (n >= 1) {
-//         score += n;
-//         n = Math.floor((n * 1) / 2);
-//     }
-//     return score;
-// }
+function halvingSum(n) {
+    let score = 0;
+    while (n >= 1) {
+        score += n;
+        n = Math.floor((n * 1) / 2);
+    }
+    return score;
+}
 
 
 // Identical Elements.
