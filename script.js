230465fca01e5c2eff20820c05c286394e0548ae
tamga05 <tamga05@gmail.com>
2023-11-19 08:08:28 +0300

    task solving

--- script.js
+++ script.js
@@ -7964,9 +7964,9 @@ Given two arrays of integers m and n, test if they contain at least one identica
 Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
 */
 
-// function duplicateElements(m, n) {
-//     return m.some((v, i, arr) => arr.includes(n[i]));
-// }
+function duplicateElements(m, n) {
+    return m.some((v, i, arr) => arr.includes(n[i]));
+}
 
 
 // Incrementer.
