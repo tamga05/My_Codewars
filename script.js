4740c0c6683ff39232cbe30bceda089058a3bc64
tamga05 <tamga05@gmail.com>
2023-11-20 05:31:49 +0300

    task solving

--- script.js
+++ script.js
@@ -7985,9 +7985,9 @@ incrementer([4,6,9,1,3]) => [5,8,2,5,8]
 Lastly, return [] if your array is empty! Arrays will only contain numbers so don't worry about checking that.
 */
 
-// function incrementer(num) {
-//     return num.map((v, i) => (v + i + 1) % 10);
-// }
+function incrementer(num) {
+    return num.map((v, i) => (v + i + 1) % 10);
+}
 
 
 // Indexed capitalization.
