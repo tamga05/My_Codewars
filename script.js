c753e4ceb40272bb4961dd3076884e8233a536a6
tamga05 <tamga05@gmail.com>
2023-12-28 07:02:52 +0300

    task solving

--- script.js
+++ script.js
@@ -8886,24 +8886,24 @@ Examples
 12 ==> 13
 */
 
-// function nextPrime(n) {
-//     let count = 0;
-//     for (let i = n + 1; i > 0; i++) {
-//         if (isPrime(i)) {
-//             count = i;
-//             break;
-//         }
-//     }
-//     return count;
-// }
-//
-// function isPrime(n) {
-//     let rt = Math.sqrt(n);
-//     for (let i = 2; i <= rt; i++) {
-//         if (n % i === 0) return false;
-//     }
-//     return n !== 1;
-// }
+function nextPrime(n) {
+    let count = 0;
+    for (let i = n + 1; i > 0; i++) {
+        if (isPrime(i)) {
+            count = i;
+            break;
+        }
+    }
+    return count;
+}
+
+function isPrime(n) {
+    let rt = Math.sqrt(n);
+    for (let i = 2; i <= rt; i++) {
+        if (n % i === 0) return false;
+    }
+    return n !== 1;
+}
 
 
 // Next birth-day of the week finder.
