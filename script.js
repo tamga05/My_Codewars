cb6a6033f6d1cb8c7f1a89de555f63af680c3636
tamga05 <tamga05@gmail.com>
2024-01-04 10:18:33 +0300

    task solving

--- script.js
+++ script.js
@@ -9036,4 +9036,79 @@ function closest(arr) {
 }
 
 
+// ToLeetSpeak.
+
+/* DESCRIPTION:
+Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+For example:
+
+toLeetSpeak("LEET") returns "1337"
+In this kata we use a simple LeetSpeak dialect. Use this alphabet:
+
+{
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2'
+} */
+
+const toLeetSpeak = s => s.replace(/[ABCEGHILOSTZ]/g, c => D[c]);
+
+const D = {
+  A: '@', B: '8', C: '(', E: '3',
+  G: '6', H: '#', I: '!', L: '1',
+  O: '0', S: '$', T: '7', Z: '2'
+};
+
+
+// Trigrams.
+
+/*
+Description:
+Trigrams are a special case of the n-gram, where n is 3. One trigram is a continious sequence of 3 chars in phrase. Wikipedia
+
+return all trigrams for the given phrase
+replace spaces with _
+return an empty string for phrases shorter than 3
+Example:
+
+trigrams('the quick red') == the he_ e_q _qu qui uic ick ck_ k_r _re red
+*/
+
+// function trigrams( phrase ) {
+//     const arr=[];
+//     for (let i=0; i<phrase.length-2;i++){
+//     arr.push(phrase.slice(i,i+3))
+//     }
+//     return arr.map(v=>v.replace(/\s/g,'_')).join(' ')
+// }
+
+
 // 
