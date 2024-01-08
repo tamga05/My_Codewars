debbda331446935e83b606518dcda2c056215866
tamga05 <tamga05@gmail.com>
2024-01-08 12:45:41 +0300

    task solving

--- script.js
+++ script.js
@@ -9034,3 +9034,70 @@ Examples:
 function closest(arr) {
   return arr.sort((a, b) => Math.abs(a) - Math.abs(b)).includes(arr[0] * -1) && arr[0] !== 0 ? null : arr[0];
 }
+
+
+// Coding 3min : A*B=C.
+
+/*
+Description:
+Coding 3min : A*B=C
+
+This is the simple version of Fastest Code series. If you need some challenges, please try the Performance version
+
+Task:
+Give you a number array numbers and a number c.
+
+Find out a pair of numbers(we called them number a and number b) from the array numbers, let a*b=c, result format is an array [a,b]
+
+The array numbers is a sorted array, value range: -100...100
+
+The result will be the first pair of numbers, for example,findAB([1,2,3,4,5,6],6) should return [1,6], instead of [2,3]
+*/
+
+// function findAB(numbers,c){
+//     for (let i=0;i<numbers.length;i++){
+//      for (let j=1+i;j<numbers.length;j++){
+//       if (numbers[i]*numbers[j]===c) return [numbers[i],numbers[j]]
+//      }
+//    }
+//   return null
+// }
+
+
+// Coding 3min : toLoverCase().
+
+/*
+Description:
+Coding 3min : toLoverCase()
+
+Task
+Yes, your eyes are no problem, this is toLoverCase (), not toLowerCase (), we want to make the world full of love.
+
+What do we need to do?
+You need to add a prototype function to the String, the name is toLoverCase. Function can convert the letters in the string, converted to "L", "O", "V", "E", if not the letter, don't change it.
+
+How to convert? see this example:
+"love!".toLoverCase()="EVOL!"
+    "l" is 11th(start with 0) in lower case letters
+    abcdefghijklmnopqrstuvwxyz
+               |
+    0...      11----------------->11th letter
+    We use this number mod 4------> 11 % 4 = 3
+
+    the 3rd(also start with 0) letter of "LOVE" is "E"
+    LOVE
+       |
+       3--------->3rd letter
+
+    so convert "l" to "E".
+
+    and so on..
+
+    "o" convert to "V", "v" convert to "O", "e" convert to "L"
+
+    last "!" remain unchanged, because it's not a letter.
+*/
+
+// String.prototype.toLoverCase=function(){
+//     return this.toLowerCase().replace(/[a-z]/g, v => 'LOVE'[(v.charCodeAt(0) - 97) % 4]).toUpperCase();
+// }
