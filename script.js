f81d87ddf676f5207c47501902a10e293d5ae475
tamga05 <tamga05@gmail.com>
2024-01-31 00:05:33 +0300

    task solving

--- script.js
+++ script.js
@@ -9456,16 +9456,16 @@ function well(x) {
 
 // What dominates your array?
 
-// function dominator(arr) {
-// 	const obj = {};
-// 	arr.map((v) => (obj[v] = obj[v] ? obj[v] + 1 : 1));
-// 	const keys = Object.keys(obj);
-// 	const value = Object.values(obj);
-// 	if (Math.max(...value) < arr.length / 2) return -1;
-// 	return value.indexOf(Math.max(...value)) === 0
-// 		? -1
-// 		: keys[value.indexOf(Math.max(...value))] * 1;
-// }
+function dominator(arr) {
+	const obj = {};
+	arr.map((v) => (obj[v] = obj[v] ? obj[v] + 1 : 1));
+	const keys = Object.keys(obj);
+	const value = Object.values(obj);
+	if (Math.max(...value) < arr.length / 2) return -1;
+	return value.indexOf(Math.max(...value)) === 0
+		? -1
+		: keys[value.indexOf(Math.max(...value))] * 1;
+}
 
 // What is my name score? #1
 
