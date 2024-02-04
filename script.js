727407e42cd55162d962b06c376429a99d82b67c
tamga05 <tamga05@gmail.com>
2024-02-04 08:03:09 +0300

    task solving

--- script.js
+++ script.js
@@ -9501,15 +9501,15 @@ function wheresWally(string) {
 
 // Alternate case.
 
-// function alternateCase(s) {
-// 	return s
-// 		.split("")
-// 		.map((value) => {
-// 			if (value != value.toUpperCase()) return value.toUpperCase();
-// 			return value.toLowerCase();
-// 		})
-// 		.join("");
-// }
+function alternateCase(s) {
+	return s
+		.split("")
+		.map((value) => {
+			if (value != value.toUpperCase()) return value.toUpperCase();
+			return value.toLowerCase();
+		})
+		.join("");
+}
 
 // makeBackronym.
 
