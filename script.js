8f5d9979318349c2bd4f202332cd1cf499506a21
tamga05 <tamga05@gmail.com>
2024-02-21 06:09:01 +0300

    task solving

--- script.js
+++ script.js
@@ -9652,12 +9652,12 @@ function spread(func, args) {
 
 // Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().
 
-// function alienLanguage(str) {
-// 	return str
-// 		.split(" ")
-// 		.map((v) => v.slice(0, -1).toUpperCase() + v.slice(-1).toLowerCase())
-// 		.join(" ");
-// }
+function alienLanguage(str) {
+	return str
+		.split(" ")
+		.map((v) => v.slice(0, -1).toUpperCase() + v.slice(-1).toLowerCase())
+		.join(" ");
+}
 
 // Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode().
 
