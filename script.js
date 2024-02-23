7da049619997fd2a454ff262e41c25073b0563df
tamga05 <tamga05@gmail.com>
2024-02-23 08:15:38 +0300

    task solving

--- script.js
+++ script.js
@@ -9741,21 +9741,21 @@ const answer3 = "Expired biscuits";
 
 // Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.
 
-// const infiniteLoop = (arr, d, n) => {
-// 	for (let i = 1; i <= n; i++) {
-// 		if (d === "left") {
-// 			arr[2].push(arr[0].shift());
-// 			arr[1].push(arr[2].shift());
-// 			arr[0].push(arr[1].shift());
-// 		}
-// 		if (d === "right") {
-// 			arr[0].unshift(arr[2].pop());
-// 			arr[1].unshift(arr[0].pop());
-// 			arr[2].unshift(arr[1].pop());
-// 		}
-// 	}
-// 	return arr;
-// };
+const infiniteLoop = (arr, d, n) => {
+	for (let i = 1; i <= n; i++) {
+		if (d === "left") {
+			arr[2].push(arr[0].shift());
+			arr[1].push(arr[2].shift());
+			arr[0].push(arr[1].shift());
+		}
+		if (d === "right") {
+			arr[0].unshift(arr[2].pop());
+			arr[1].unshift(arr[0].pop());
+			arr[2].unshift(arr[1].pop());
+		}
+	}
+	return arr;
+};
 
 // Training JS #24: methods of arrayObject---splice() and slice().
 
