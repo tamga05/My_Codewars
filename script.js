00c33591d058b1257c0bd349cba3c85f3f08fdfb
tamga05 <tamga05@gmail.com>
2024-02-24 07:38:50 +0300

    task solving

--- script.js
+++ script.js
@@ -9759,13 +9759,13 @@ const infiniteLoop = (arr, d, n) => {
 
 // Training JS #24: methods of arrayObject---splice() and slice().
 
-// function threeInOne(arr) {
-// 	const arr1 = [];
-// 	for (let i = 0; i <= arr.length; i += 3) {
-// 		arr1.push(arr.slice(i, i + 3));
-// 	}
-// 	return arr1.map((v) => v.reduce((a, b) => a + b, 0)).slice(0, -1);
-// }
+function threeInOne(arr) {
+	const arr1 = [];
+	for (let i = 0; i <= arr.length; i += 3) {
+		arr1.push(arr.slice(i, i + 3));
+	}
+	return arr1.map((v) => v.reduce((a, b) => a + b, 0)).slice(0, -1);
+}
 
 // Training JS #26: methods of arrayObject---map().
 
