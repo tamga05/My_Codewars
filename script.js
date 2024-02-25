af293f701733b94add617d17995757abb7524491
tamga05 <tamga05@gmail.com>
2024-02-25 08:37:58 +0300

    task solving

--- script.js
+++ script.js
@@ -9769,16 +9769,16 @@ function threeInOne(arr) {
 
 // Training JS #26: methods of arrayObject---map().
 
-// function isolateIt(arr) {
-// 	return arr.map((v) => {
-// 		if (v.length % 2 == 0) {
-// 			return v.slice(0, v.length / 2) + "|" + v.slice(v.length / 2);
-// 		}
-// 		if (v.length % 2 !== 0) {
-// 			return v.slice(0, v.length / 2) + "|" + v.slice(v.length / 2 + 1);
-// 		}
-// 	});
-// }
+function isolateIt(arr) {
+	return arr.map((v) => {
+		if (v.length % 2 == 0) {
+			return v.slice(0, v.length / 2) + "|" + v.slice(v.length / 2);
+		}
+		if (v.length % 2 !== 0) {
+			return v.slice(0, v.length / 2) + "|" + v.slice(v.length / 2 + 1);
+		}
+	});
+}
 
 // Training JS #27: methods of arrayObject---filter().
 
