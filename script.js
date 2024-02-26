35846a796a014a9274406b44b6bfe2e1a2bb84a6
tamga05 <tamga05@gmail.com>
2024-02-26 05:25:41 +0300

    task solving

--- script.js
+++ script.js
@@ -9782,30 +9782,30 @@ function isolateIt(arr) {
 
 // Training JS #27: methods of arrayObject---filter().
 
-// function countGrade(scores) {
-// 	const obj = { S: 0, A: 0, B: 0, C: 0, D: 0, X: 0 };
-// 	scores.map((v) => {
-// 		if (v >= 100) {
-// 			obj[`S`]++;
-// 		}
-// 		if (v >= 90 && v < 100) {
-// 			obj[`A`]++;
-// 		}
-// 		if (v >= 80 && v < 90) {
-// 			obj[`B`]++;
-// 		}
-// 		if (v >= 60 && v < 80) {
-// 			obj[`C`]++;
-// 		}
-// 		if (v >= 0 && v < 60) {
-// 			obj[`D`]++;
-// 		}
-// 		if (v < 0) {
-// 			obj[`X`]++;
-// 		}
-// 	});
-// 	return obj;
-// }
+function countGrade(scores) {
+	const obj = { S: 0, A: 0, B: 0, C: 0, D: 0, X: 0 };
+	scores.map((v) => {
+		if (v >= 100) {
+			obj[`S`]++;
+		}
+		if (v >= 90 && v < 100) {
+			obj[`A`]++;
+		}
+		if (v >= 80 && v < 90) {
+			obj[`B`]++;
+		}
+		if (v >= 60 && v < 80) {
+			obj[`C`]++;
+		}
+		if (v >= 0 && v < 60) {
+			obj[`D`]++;
+		}
+		if (v < 0) {
+			obj[`X`]++;
+		}
+	});
+	return obj;
+}
 
 // Training JS #28: methods of arrayObject---every() and some().
 
