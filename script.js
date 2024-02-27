c18331b6de93e169781394f777b8bd711c523385
tamga05 <tamga05@gmail.com>
2024-02-27 05:31:17 +0300

    task solving

--- script.js
+++ script.js
@@ -9809,17 +9809,17 @@ function countGrade(scores) {
 
 // Training JS #28: methods of arrayObject---every() and some().
 
-// function mirrorImage(arr) {
-// 	for (let i = 0; i < arr.length - 1; i++) {
-// 		if (
-// 			arr[i].toString().split("").reverse().join("") ===
-// 			arr[i + 1].toString()
-// 		) {
-// 			return [arr[i], arr[i + 1]];
-// 		}
-// 	}
-// 	return [-1, -1];
-// }
+function mirrorImage(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (
+			arr[i].toString().split("").reverse().join("") ===
+			arr[i + 1].toString()
+		) {
+			return [arr[i], arr[i + 1]];
+		}
+	}
+	return [-1, -1];
+}
 
 // Training JS #30: methods of arrayObject---reduce() and reduceRight().
 
