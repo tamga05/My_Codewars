11166aca20e9f274c1900df622722b55b51579f1
tamga05 <tamga05@gmail.com>
2024-03-04 06:26:23 +0300

    task solving

--- script.js
+++ script.js
@@ -9871,19 +9871,19 @@ function vowel2index(str) {
 
 // The Pony Express.
 
-// function riders(stations) {
-// 	let riders = 0;
-// 	let sum = 0;
-// 	for (let i = 0; i < stations.length; i++) {
-// 		sum += stations[i];
-// 		if (sum > 100) {
-// 			riders++;
-// 			sum = 0;
-// 			i--;
-// 		}
-// 	}
-// 	return riders + 1;
-// }
+function riders(stations) {
+	let riders = 0;
+	let sum = 0;
+	for (let i = 0; i < stations.length; i++) {
+		sum += stations[i];
+		if (sum > 100) {
+			riders++;
+			sum = 0;
+			i--;
+		}
+	}
+	return riders + 1;
+}
 
 // TV Remote.
 
