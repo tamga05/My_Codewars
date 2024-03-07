89317f8c299d0b9b71a365605a2a0a18ea0675b9
tamga05 <tamga05@gmail.com>
2024-03-07 06:28:42 +0300

    task solving

--- script.js
+++ script.js
@@ -9935,21 +9935,21 @@ function tailSwap(arr) {
 
 // Tax Calculator.
 
-// function taxCalculator(total) {
-// 	let tax = 0;
-// 	if (total <= 0 || isNaN(total)) {
-// 		return 0;
-// 	} else if (total > 0 && total <= 10) {
-// 		tax = total * 0.1;
-// 	} else if (total > 10 && total <= 20) {
-// 		tax = 1 + (total - 10) * 0.07;
-// 	} else if (total > 20 && total <= 30) {
-// 		tax = 1.7 + (total - 20) * 0.05;
-// 	} else {
-// 		tax = 2.2 + (total - 30) * 0.03;
-// 	}
-// 	return +tax.toFixed(2);
-// }
+function taxCalculator(total) {
+	let tax = 0;
+	if (total <= 0 || isNaN(total)) {
+		return 0;
+	} else if (total > 0 && total <= 10) {
+		tax = total * 0.1;
+	} else if (total > 10 && total <= 20) {
+		tax = 1 + (total - 10) * 0.07;
+	} else if (total > 20 && total <= 30) {
+		tax = 1.7 + (total - 20) * 0.05;
+	} else {
+		tax = 2.2 + (total - 30) * 0.03;
+	}
+	return +tax.toFixed(2);
+}
 
 // Tea for two.
 
