d4dc738e870445cbfab073b91e39e701c67dedd5
tamga05 <tamga05@gmail.com>
2024-03-13 11:46:22 +0300

    task solving

--- script.js
+++ script.js
@@ -10008,13 +10008,13 @@ function arpeggio(notes) {
 
 // The Barksdale Code.
 
-// function decode(string) {
-// 	const obj = { 1: 9, 2: 8, 3: 7, 4: 6, 5: 0, 6: 4, 7: 3, 8: 2, 9: 1, 0: 5 };
-// 	return string
-// 		.split("")
-// 		.map((v) => (v = obj[v]))
-// 		.join("");
-// }
+function decode(string) {
+	const obj = { 1: 9, 2: 8, 3: 7, 4: 6, 5: 0, 6: 4, 7: 3, 8: 2, 9: 1, 0: 5 };
+	return string
+		.split("")
+		.map((v) => (v = obj[v]))
+		.join("");
+}
 
 // The Coins of Ter | Round to the Next N.
 
