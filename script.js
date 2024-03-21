320e3f1b5c83b5cb8ce3387409b621831f4403ca
tamga05 <tamga05@gmail.com>
2024-03-21 07:02:17 +0300

    task solving

--- script.js
+++ script.js
@@ -10082,18 +10082,18 @@ function boredom(staff) {
 		: "i can handle this";
 }
 
-// const obj = {
-// 	accounts: 1,
-// 	finance: 2,
-// 	canteen: 10,
-// 	regulation: 3,
-// 	trading: 6,
-// 	change: 6,
-// 	IS: 8,
-// 	retail: 5,
-// 	cleaning: 4,
-// 	"pissing about": 25,
-// };
+const obj = {
+	accounts: 1,
+	finance: 2,
+	canteen: 10,
+	regulation: 3,
+	trading: 6,
+	change: 6,
+	IS: 8,
+	retail: 5,
+	cleaning: 4,
+	"pissing about": 25,
+};
 
 // The Office III - Broken Photocopier.
 
