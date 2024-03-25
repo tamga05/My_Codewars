3fec442be417f77d797d368e0f58d845a19dc38a
tamga05 <tamga05@gmail.com>
2024-03-25 07:14:05 +0300

    task solving

--- script.js
+++ script.js
@@ -10119,16 +10119,16 @@ function sabb(x, val, happ) {
 
 // The Owls Are Not What They Seem.
 
-// function owlPic(text) {
-// 	const dict = ["8", "W", "T", "Y", "U", "I", "O", "A", "H", "X", "V", "M"];
-// 	const left = text
-// 		.toUpperCase()
-// 		.split("")
-// 		.filter((v) => dict.includes(v))
-// 		.join("");
-// 	const right = left.split("").reverse().join("");
-// 	return left + `''0v0''` + right;
-// }
+function owlPic(text) {
+	const dict = ["8", "W", "T", "Y", "U", "I", "O", "A", "H", "X", "V", "M"];
+	const left = text
+		.toUpperCase()
+		.split("")
+		.filter((v) => dict.includes(v))
+		.join("");
+	const right = left.split("").reverse().join("");
+	return left + `''0v0''` + right;
+}
 
 // The Span Function.
 
