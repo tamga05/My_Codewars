bfb249e9f21c525a42887c807122ff1b9f369c95
tamga05 <tamga05@gmail.com>
2024-03-28 13:54:10 +0300

    task solving

--- script.js
+++ script.js
@@ -10148,23 +10148,23 @@ function minMax(arr) {
 
 // The mean of two means.
 
-// function getMean(arr, x, y) {
-// 	if (x > 1 && y > 1 && x <= arr.length && y <= arr.length) {
-// 		var mean1 = 0,
-// 			mean2 = 0,
-// 			answer;
-// 		var clone = arr.slice().reverse();
-// 		for (var i = 0; i < x; i++) {
-// 			mean1 += arr[i];
-// 		}
-// 		for (var j = 0; j < y; j++) {
-// 			mean2 += clone[j];
-// 		}
-// 		answer = (mean1 / x + mean2 / y) / 2;
-// 		return answer;
-// 	}
-// 	return -1;
-// }
+function getMean(arr, x, y) {
+	if (x > 1 && y > 1 && x <= arr.length && y <= arr.length) {
+		var mean1 = 0,
+			mean2 = 0,
+			answer;
+		var clone = arr.slice().reverse();
+		for (var i = 0; i < x; i++) {
+			mean1 += arr[i];
+		}
+		for (var j = 0; j < y; j++) {
+			mean2 += clone[j];
+		}
+		answer = (mean1 / x + mean2 / y) / 2;
+		return answer;
+	}
+	return -1;
+}
 
 // The once rollable dice.
 
