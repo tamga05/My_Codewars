a2224f7e29a5ac3300ebd0c5b5e6d338992cc202
tamga05 <tamga05@gmail.com>
2024-03-29 06:13:43 +0300

    task solving

--- script.js
+++ script.js
@@ -10168,13 +10168,13 @@ function getMean(arr, x, y) {
 
 // The once rollable dice.
 
-// function Character(name, opness) {
-// 	this.name = name;
-// 	this.opness = opness;
-// 	this.attack = function () {
-// 		return this.opness + ~~(Math.random() * 20) + 1;
-// 	};
-// }
+function Character(name, opness) {
+	this.name = name;
+	this.opness = opness;
+	this.attack = function () {
+		return this.opness + ~~(Math.random() * 20) + 1;
+	};
+}
 
 // The reject() function.
 
