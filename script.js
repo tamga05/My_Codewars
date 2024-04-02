98059a2597769f26fc5a6c9c71ffa8dbe7e40549
tamga05 <tamga05@gmail.com>
2024-04-02 07:25:27 +0300

    task solving

--- script.js
+++ script.js
@@ -10191,17 +10191,17 @@ function theVar(theVariables) {
 
 // The wheat rice and chessboard problem.
 
-// function squaresNeeded(grains) {
-// 	const arr = [0];
-// 	for (let i = 0; i < 63; i++) {
-// 		arr.push(Math.pow(2, i));
-// 	}
-// 	for (let i = 0; i < 63; i++) {
-// 		if (grains < arr[i]) {
-// 			return i - 1;
-// 		}
-// 	}
-// }
+function squaresNeeded(grains) {
+	const arr = [0];
+	for (let i = 0; i < 63; i++) {
+		arr.push(Math.pow(2, i));
+	}
+	for (let i = 0; i < 63; i++) {
+		if (grains < arr[i]) {
+			return i - 1;
+		}
+	}
+}
 
 // Thinkful - Logic Drills: Umbrella decider.
 
