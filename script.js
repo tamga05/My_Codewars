399a97fd851fa19edfeaac08eb8bd26c53b54678
tamga05 <tamga05@gmail.com>
2024-04-12 16:02:23 +0300

    task solving

--- script.js
+++ script.js
@@ -10270,9 +10270,9 @@ function testit(s) {
 
 // Thinking & Testing : Math of Primary School.
 
-// function testit(arr) {
-// 	return arr[0] * arr[3] + arr[1] * arr[2];
-// }
+function testit(arr) {
+	return arr[0] * arr[3] + arr[1] * arr[2];
+}
 
 // Thinking & Testing : Retention and discard.
 
