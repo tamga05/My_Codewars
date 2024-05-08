643f3b27292733758a34ca1d299aad58d2a899c2
tamga05 <tamga05@gmail.com>
2024-05-08 13:39:03 +0300

    task solving

--- script.js
+++ script.js
@@ -10450,16 +10450,16 @@ const paintLetterboxes = (start, end) => {
 
 // Linked Lists - Get Nth.
 
-// function Node(data) {
-// 	this.data = data;
-// 	this.next = null;
-// }
+function Node(data) {
+	this.data = data;
+	this.next = null;
+}
 
-// function getNth(node, index) {
-// 	if (!node) throw new Error("Invalid node at " + index);
+function getNth(node, index) {
+	if (!node) throw new Error("Invalid node at " + index);
 
-// 	return index === 0 ? node : getNth(node.next, index - 1);
-// }
+	return index === 0 ? node : getNth(node.next, index - 1);
+}
 
 // Linked Lists - Move Node.
 
